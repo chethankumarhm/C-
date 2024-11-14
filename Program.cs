d65021ea3493b856282cbea02158ba8c70d7d58d
chethankumarhm <chethankumarhmm@gmail.com>
2024-11-14 17:42:09 +0530

    Program file added

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics.Metrics;
+
+namespace practice
+{
+    class Program
+    {
+
+        static int a = 10;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Answer is = "+ a );
+        }
+    }
+
+}
